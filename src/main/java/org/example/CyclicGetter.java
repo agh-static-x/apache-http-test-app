833dbb200edaaf01eb301db0fb9638518a66cbf5
enkelian <aniano13@gmail.com>
2021-10-09 19:03:29 +0200

    sample app with plugin config

---
+++ src/main/java/org/example/CyclicGetter.java
@@ -0,0 +1,73 @@
+package org.example;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class CyclicGetter {
+
+    public static void main(String[] args){
+        OtelConfig.configureOtel();
+
+        while(true){
+            try {
+                makeCall();
+                Thread.sleep(2000);
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+                System.out.println();
+            } catch (InterruptedException e) {
+                System.out.println("Problem with sleep :(");
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private static void makeCall() throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        try {
+
+            HttpGet request = new HttpGet("https://httpbin.org/get");
+
+            // add request headers
+            request.addHeader("custom-key", "mkyong");
+            request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
+
+            CloseableHttpResponse response = httpClient.execute(request);
+
+            try {
+
+                // Get HttpResponse Status
+                System.out.println(response.getProtocolVersion());              // HTTP/1.1
+                System.out.println(response.getStatusLine().getStatusCode());   // 200
+                System.out.println(response.getStatusLine().getReasonPhrase()); // OK
+                System.out.println(response.getStatusLine().toString());        // HTTP/1.1 200 OK
+
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    // return it as a String
+                    String result = EntityUtils.toString(entity);
+                    System.out.println(result);
+                }
+
+            } finally {
+                response.close();
+            }
+        } finally {
+            httpClient.close();
+        }
+    }
+
+
+
+
+}
---
+++ src/main/java/org/example/OtelConfig.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.zipkin.ZipkinSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+
+public class OtelConfig {
+
+    public static void configureOtel() {
+
+        ZipkinSpanExporter exporter = ZipkinSpanExporter.builder().setEndpoint("http://localhost:9411").build();
+
+        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+                .addSpanProcessor(BatchSpanProcessor.builder(exporter).build())
+                .build();
+
+        OpenTelemetrySdk.builder()
+                .setTracerProvider(sdkTracerProvider)
+                .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+                .buildAndRegisterGlobal();
+
+        System.out.println("OTEL CONFIGURED");
+    }
+}
