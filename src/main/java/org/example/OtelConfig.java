4bf428c9a262bf49820b6e58cf82377904191822
enkelian <aniano13@gmail.com>
2021-10-12 17:14:59 +0200

    classloading

--- src/main/java/org/example/OtelConfig.java
+++ src/main/java/org/example/OtelConfig.java
@@ -6,15 +6,16 @@ import io.opentelemetry.exporter.zipkin.ZipkinSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.exporter.logging.LoggingSpanExporter;
 
 public class OtelConfig {
 
     public static void configureOtel() {
 
-        ZipkinSpanExporter exporter = ZipkinSpanExporter.builder().setEndpoint("http://localhost:9411").build();
+//        ZipkinSpanExporter exporter = ZipkinSpanExporter.builder().setEndpoint("http://localhost:9411").build();
 
         SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
-                .addSpanProcessor(BatchSpanProcessor.builder(exporter).build())
+                .addSpanProcessor(BatchSpanProcessor.builder(new LoggingSpanExporter()).build())
                 .build();
 
         OpenTelemetrySdk.builder()
